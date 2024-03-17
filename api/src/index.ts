891981287a683644e02a7e4e701f578d9a541b30
José Brito <josealbritose@gmail.com>
2024-03-16 22:03:06 -0300

    mongoose orm install - connect mongoDB

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,8 @@
 import startApolloServer  from "./app";
 import { typeDefs,resolvers } from "./graphql/index";
+import { connectDB } from "./config/db";
+
+connectDB();
 
 startApolloServer(typeDefs,resolvers)
 
