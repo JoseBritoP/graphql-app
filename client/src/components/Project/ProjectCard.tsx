1c62664ad7b09236e3cb92b431287292c570fa65
José Brito <josealbritose@gmail.com>
2024-03-19 17:54:17 -0300

    DELETE PROJECT

--- client/src/components/Project/ProjectCard.tsx
+++ client/src/components/Project/ProjectCard.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom"
+import { useMutation } from "@apollo/client"
+import { DELETE_PROJECT } from "../../graphql/projects"
 
 interface Props {
   _id:string,
@@ -10,10 +12,25 @@ interface Props {
 
 export default function ProjectCard({_id,name,description}:Props) {
   const navigate = useNavigate();
+
+  const [deleteProject] = useMutation(DELETE_PROJECT,{
+    refetchQueries:["getProjects"]
+  });
+
+  const handleDelete = () => {
+    deleteProject({
+      variables:{
+        id:_id
+      }
+    })
+  };
   return (
-    <div key={_id} onClick={()=>navigate(`/projects/detail/${_id}`)} className="bg-slate-950 p-4 rounded-md hover:bg-slate-900 hover:cursor-pointer ease-in-out shadow-lg shadow-black">
-      <h2 className="text-xl font-semibold">{name}</h2>
-      <p className="text-lg text-slate-300">{!description ? 'No description' : description }</p>
+    <div key={_id}  className="bg-slate-950 p-4 rounded-md hover:bg-slate-900 hover:cursor-pointer ease-in-out shadow-lg shadow-black flex justify-between">
+      <div onClick={()=>navigate(`/projects/detail/${_id}`)} className="w-full">
+        <h2 className="text-xl font-semibold">{name}</h2>
+        <p className="text-lg text-slate-300">{!description ? 'No description' : description }</p>
+      </div>
+      <button className="bg-red-500 hover:bg-red-600 px-2 py-1 rounded-md" onClick={handleDelete}>Delete</button>
     </div>
   )
 }
