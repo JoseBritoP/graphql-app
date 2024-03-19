87b3d05b931e820fda5bb6b95602746ef902e51e
José Brito <josealbritose@gmail.com>
2024-03-19 17:54:41 -0300

    Styles

--- client/src/components/Task/TaskForm.tsx
+++ client/src/components/Task/TaskForm.tsx
@@ -35,9 +35,11 @@ export default function TaskForm() {
     e.target.title.focus();
   }
   return (
-    <form onSubmit={handleSubmit} className='flex items-center gap-x-6'>
-      <label htmlFor="title">Title</label>
-      <input type="text" name="title" id="title" className='text-lg px-2 rounded-sm text-black' placeholder='Title Task' onChange={handleChange} />
+    <form onSubmit={handleSubmit} className='flex items-center justify-between w-full gap-x-6'>
+      <div className='flex gap-x-4'>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" id="title" className='text-lg px-2 rounded-sm text-black' placeholder='Title Task' onChange={handleChange} />
+      </div>
       <button className='bg-emerald-600 hover:bg-emerald-700 p-1 rounded-sm px-2 disabled:bg-zinc-600 disabled:hover:cursor-default' disabled={!form.title}>Add tasks</button>
     </form>
   )
