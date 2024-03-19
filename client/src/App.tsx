91fcc5286d0abb28f82035696c2431aec61c234e
José Brito <josealbritose@gmail.com>
2024-03-18 21:44:46 -0300

    Apollo client init - Routing init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter,Routes,Route, Navigate } from 'react-router-dom';
+import { ProjectDetail, Projects, ErrorPage } from './views';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <div >
-      <h1>Hello wolrd</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to={'/projects'}/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/projects/detail/:id' element={<ProjectDetail/>}/>
+          <Route path='/*' element={<ErrorPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
---
+++ client/src/views/index.ts
@@ -0,0 +1,6 @@
+import ProjectDetail from "./ProjectDetail";
+import Projects from "./Projects";
+import ErrorPage from "./ErrorPage";
+export {
+  ProjectDetail,Projects,ErrorPage
+}
